411b301fee0485f1ff0769e61aff2b36a1154d1f
anhnt <tien226anh@gmail.com>
2024-03-16 22:32:50 +0700

    add

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,7 +2,6 @@ import { Home, Calendar, Discover, Personal } from "@/screens";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 
-
 import { HomeIcon as HomeUnactive } from "react-native-heroicons/outline";
 import { HomeIcon as HomeActive } from "react-native-heroicons/solid";
 
@@ -18,15 +17,22 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types";
 import PlanOverviewScreen from "@/screens/PlanOverviewScreen";
 
-
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function StackScreen(){
+function StackScreen() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-      <Stack.Screen name="PlanOverview" component={PlanOverviewScreen} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PlanOverview"
+        component={PlanOverviewScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
@@ -34,29 +40,35 @@ function StackScreen(){
 export function AppNavigator() {
   return (
     <Tab.Navigator
-
       initialRouteName="Home"
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconSource;
 
-          if (route.name === "Home") {
-            iconSource = focused
-              ? <HomeActive color="white" fill="blue" size={20} />
-              : <HomeUnactive color="white" size={20} />;
-          }
-          else if (route.name === "Discover") {
-            iconSource = focused
-              ? <DiscoverActive color="white" fill="blue" size={20} />
-              : <DiscoverUnactive color="white" size={20} />;
+          if (route.name === "StackScreen") {
+            iconSource = focused ? (
+              <HomeActive color="white" fill="blue" size={20} />
+            ) : (
+              <HomeUnactive color="white" size={20} />
+            );
+          } else if (route.name === "Discover") {
+            iconSource = focused ? (
+              <DiscoverActive color="white" fill="blue" size={20} />
+            ) : (
+              <DiscoverUnactive color="white" size={20} />
+            );
           } else if (route.name === "Calendar") {
-            iconSource = focused
-              ? <CalendarActive color="white" fill="blue" size={20} />
-              : <CalendarUnactive color="white" size={20} />;
+            iconSource = focused ? (
+              <CalendarActive color="white" fill="blue" size={20} />
+            ) : (
+              <CalendarUnactive color="white" size={20} />
+            );
           } else if (route.name === "Personal") {
-            iconSource = focused
-              ? <PersonalActive color="white" fill="blue" size={20} />
-              : <PersonalUnactive color="white" size={20} />;
+            iconSource = focused ? (
+              <PersonalActive color="white" fill="blue" size={20} />
+            ) : (
+              <PersonalUnactive color="white" size={20} />
+            );
           }
 
           return iconSource;
@@ -66,19 +78,20 @@ export function AppNavigator() {
           // );
         },
         tabBarStyle: {
-          backgroundColor: 'black', // Background color of the tab bar
+          backgroundColor: "black", // Background color of the tab bar
           elevation: 0,
           shadowOffset: {
-            width: 0, height: 0
+            width: 0,
+            height: 0,
           },
-          borderTopWidth: 0
+          borderTopWidth: 0,
         },
-        tabBarActiveTintColor: 'white', // Color of the active tab icon
+        tabBarActiveTintColor: "white", // Color of the active tab icon
       })}
-    //
+      //
     >
       <Tab.Screen
-        name="Home"
+        name="StackScreen"
         component={StackScreen}
         options={{ headerShown: false }}
       />
@@ -99,7 +112,5 @@ export function AppNavigator() {
         options={{ headerShown: false }}
       />
     </Tab.Navigator>
-
   );
-  
 }
