2320b4b476ee1a8f880835180f752e3adc3d6443
anhnt <tien226anh@gmail.com>
2024-03-18 17:23:03 +0700

    fix

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,5 @@
 export { Login } from '@/screens/Login/Login';
 export { Discover } from '@/screens/Discover/Discover';
-export { Calendar } from '@/screens/Calendar/Calendar';
 export { Personal } from '@/screens/Personal/Personal';
 export { Home } from '@/screens/Home/Home';
 export { WorkoutSetting } from '@/screens/WorkoutSetting/WorkoutSetting';
