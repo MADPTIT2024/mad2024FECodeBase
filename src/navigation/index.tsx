193a98ecc825851fba9932a306599c96350862ab
B20DCCN520_Hoàng Duy Phương <hoangduyphuong30102002@gmail.com>
2024-05-05 23:54:22 +0700

    login

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,13 @@
-import { AppNavigator } from "@/navigation/AppNavigator";
-import { NavigationContainer } from "@react-navigation/native";
-import { ColorSchemeName, useColorScheme } from "react-native";
-import { DefaultTheme } from "@react-navigation/native";
-import Colors from "@/constants/Colors";
+import { AppNavigator } from '@/navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { ColorSchemeName, useColorScheme } from 'react-native';
+import { DefaultTheme } from '@react-navigation/native';
+import Colors from '@/constants/Colors';
+import { Login } from '@/screens';
+import { useState } from 'react';
 
 export function RootNavigator() {
-  let scheme: NonNullable<ColorSchemeName> = useColorScheme() || "light";
+  let scheme: NonNullable<ColorSchemeName> = useColorScheme() || 'light';
 
   const theme = {
     ...DefaultTheme,
@@ -14,10 +16,19 @@ export function RootNavigator() {
       background: Colors.background,
     },
   };
-  
+
+  const [loginRoot, setLoginRoot] = useState(false);
+
+  function handleLoginRoot(loggedIn: boolean) {
+    // Nhận giá trị `true` từ component con và thực hiện các thao tác cần thiết ở đây
+    console.log('User logged in:', loggedIn);
+    setLoginRoot(loggedIn);
+  }
+
   return (
     <NavigationContainer theme={theme}>
-      <AppNavigator />
+      {!loginRoot && <Login loginRoot={handleLoginRoot} />}
+      {loginRoot && <AppNavigator />}
     </NavigationContainer>
   );
 }
